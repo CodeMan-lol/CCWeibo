c50a6c11836534a3b31c2895cf602a94da9baf67
cache <cache0928@hotmail.com>
2016-02-03 10:17:44 +0800

    初始化工程结构
    添加各个tabVC

---
+++ CCWeibo/CCWeibo/Classes/Main/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  CCWeibo
+//
+//  Created by 徐才超 on 16/2/2.
+//  Copyright © 2016年 徐才超. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tabBar.tintColor = UIColor.orangeColor()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
